a2c6b0045ba5b53bbd5a8a21963cac3aa73469da
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-07-11 17:07:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,34 +9,42 @@ data "azurerm_key_vault_secret" "clientid" {
   name         = "clientid"
   key_vault_id = data.azurerm_key_vault.mysecretsfiles.id
 }
-
+output "clientid"{
+value = data.azurerm_key_vault_secret.clientid.value
+}
 data "azurerm_key_vault_secret" "clientSecret" {
   name         = "clientSecret"
   key_vault_id = data.azurerm_key_vault.mysecretsfiles.id
 }
-
+output "clientSecret"{
+value = data.azurerm_key_vault_secret.clientSecret.value
+}
 data "azurerm_key_vault_secret" "subscriptionID" {
   name         = "subscriptionID"
   key_vault_id = data.azurerm_key_vault.mysecretsfiles.id
+}
+output "subscriptionID" {
+value = data.azurerm_key_vault_secret.subscriptionID.value
 }
 data "azurerm_key_vault_secret" "tenantid" {
   name         = tenantid
   key_vault_id = data.azurerm_key_vault.mysecretsfiles.id
 }
 
-output "secret_value" {
+output "tenantid" {
   value = data.azurerm_key_vault_secret.tenantid.value
-  value = data.azurerm_key_vault_secret.subscriptionID.value
-value = data.azurerm_key_vault_secret.clientid.value
-value = data.azurerm_key_vault_secret.clientSecret.value
 }
+  subscription_id = value.subscriptionID
+  client_id       = value.clientid
+  client_secret   = value.clientid
+  tenant_id       = value.tenantid
 
 resource "azurerm_resource_group" "k8s" {
   name     = var.resource_group_name
   location = var.location
 }
 module "network" {
-  source    = "../network"
+  source    = "./network"
   vnet_name =   var.vnet_name
 resource_group_name = azurerm_resource_group.k8s.name
    subnet_address_space    = [var.subnet_address_space]
